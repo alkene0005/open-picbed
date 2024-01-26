f93c899823c3352109e659fb27aa3bc17274b788
Alken E <61105068+alkene0005@users.noreply.github.com>
2024-01-27 00:07:22 +0800

    Upload file

---
+++ tampermonkey/baidux.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Baidu Test
+// @namespace    http://tampermonkey.net/
+// @version      3
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.baidu.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=baidu.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    alert('3')
+})();
