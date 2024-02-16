5e333fc4c77dce8efab930519e722bd2dca50c40
Alken E <61105068+alkene0005@users.noreply.github.com>
2024-02-16 14:08:51 +0800

    Upload file

--- tmp/baidu.js
+++ tmp/baidu.js
@@ -1,17 +1,26 @@
 // ==UserScript==
 // @name         Baidu Test
 // @namespace    http://tampermonkey.net/
-// @version      4
+// @version      5
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.baidu.com/
+// @match        https://www.bilibili.com/v/popular/rank/all
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=baidu.com
 // @grant        none
 // ==/UserScript==
+
+
 
 (function() {
     'use strict';
 
     // Your code here...
-    alert('2')
+    const lis = Array.from(document.querySelectorAll('#app > div > div.rank-container > div.rank-list-wrap > ul > li'))
+
+    lis.filter((x, i) => {
+        let title = x.querySelector('a.title').innerText
+        let up = x.querySelector('span.up-name').innerText
+        return title.includes('原神') || up.includes('Warma')
+        // 星穹铁道
+    }).forEach(x => x.remove())
 })();
